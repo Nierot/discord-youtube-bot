e889f920e455edd7255ebfd748a48039ba4ae07c
Niels <nielsrotmensen@outlook.com>
2020-04-04 16:48:35 +0200

    It can now search and play music

--- cogs/youtube.py
+++ cogs/youtube.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 from discord.ext import commands
 import urllib.request
 import urllib
@@ -5,13 +6,16 @@ import discord
 from bs4 import BeautifulSoup
 import youtube_dl
 import asyncio
+import os
+
 
 
 class Youtube(commands.Cog, name='youtube'):
 
     def __init__(self, bot):
         self.bot = bot
-        self.voice_client = None
+        self.voice_client = {}
+        self.playing = {}
 
 
     @commands.command(name="search", aliases=['zoek', 'queue'])
@@ -29,6 +33,7 @@ class Youtube(commands.Cog, name='youtube'):
         try:
             await ctx.send("Searching for {}".format(" ".join(args)))
             link = await self._search(args, 1)
+            song_id = link[0].replace('https://www.youtube.com/watch?v=', '')
 
             song = BeautifulSoup(urllib.request.urlopen(link[0]), "lxml")
             await self._set_playing_status(song.title.string.replace('- YouTube', ''))
@@ -36,21 +41,28 @@ class Youtube(commands.Cog, name='youtube'):
             await ctx.send("Playing {}".format(link[0]))
             await self._download(link[0])
 
+            if ctx.message.author.voice is None:
+                await ctx.send("You are not in a voice channel!")
+                raise Exception('Not in a voice channel')
             channel = ctx.message.author.voice.channel
-            if channel is None:
-                ctx.send("You are not in a voice channel!")
+            guild = ctx.message.author.guild.id
 
-            await self._join_channel(channel)
-            await self._play_audio('CHRISTOPHER BOWES AND HIS PLATE OF BEANS - Midnight Breakfast _ Napalm Records-HR6kOHccYRY.webm')
+            await self._join_channel(guild, channel)
+            self.playing[guild] = song_id
+            #song_file = os.path
+            await self._play_audio(guild, 'music/{}.webm'.format(song_id))
 
-            #audio_source = discord.FFmpegPCMAudio('CHRISTOPHER BOWES AND HIS PLATE OF BEANS - Midnight Breakfast _ Napalm Records-HR6kOHccYRY.webm')
-        except Exception as e:
-            await ctx.send(e)
+        except discord.ClientException:
+            await ctx.send("Already playing something... please try again")
+            await self._leave_channel(self.voice_client[guild].disconnect())
+        # except Exception as e:
+        #     await ctx.send(e)
+        #     print(e)
 
     
     @commands.command(name="disconnect", hidden=True)
     async def disconnect(self, ctx):
-        await self._leave_channel()
+        await self._leave_channel(ctx.message.author.guild.id)
 
     async def _search(self, keyword: [], amount):
         keyword = ' '.join(keyword)
@@ -73,20 +85,23 @@ class Youtube(commands.Cog, name='youtube'):
 
     async def _download(self, link):
         ydl_opts = {
-            'extractaudio': True,
-            'keepvideo': False,
+            'extractaudio': 'True',
+            'keepvideo': 'False',
+            'forcefilename': 'True',
+            'audio-format': 'mp3',
             'format': 'bestaudio/best',
+            'outtmpl': '/music/%(id)s.%(ext)s',
         }
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             ydl.download([link])
 
 
-    async def _join_channel(self, channel):
-        self.voice_client = await channel.connect()
+    async def _join_channel(self, guild, channel):
+        self.voice_client[guild] = await channel.connect()
 
     
-    async def _leave_channel(self):
-        await self.voice_client.disconnect()
+    async def _leave_channel(self, guild):
+        await self.voice_client[guild].disconnect()
 
 
     async def _set_playing_status(self, music_name):
@@ -98,10 +113,10 @@ class Youtube(commands.Cog, name='youtube'):
         src.read()
         return src
 
-    async def _play_audio(self, audio):
+    async def _play_audio(self, guild, audio):
         source = await self._new_audio_source(audio)
         try:
-            self.voice_client.play(source)
+            self.voice_client[guild].play(source)
         except discord.ClientException as ce:
             print("Already playing audio or not connected")
         except Exception as e:
